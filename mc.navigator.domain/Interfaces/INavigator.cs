5c435322aa23eca33237d8233877bda588266c15
Willimar <willimar@gmail.com>
2019-05-23 22:08:02 -0300

    Add files via upload

---
+++ mc.navigator.domain/Interfaces/INavigator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace mc.navigator.domain.Interfaces
+{
+    public enum Method
+    {
+        get,
+        post
+    }
+
+    public interface INavigator
+    {
+        Method RequestMethod { get; set; }
+        HttpRequestHeaders Headers { get; }
+        int TimeOut { get; set; }
+        Dictionary<string, string> Form { get; set; }
+
+        string Navigate(Uri uri, Method method);
+    }
+}
---
+++ mc.navigator.domain/Interfaces/IProviderService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mc.navigator.domain.Interfaces
+{
+    public interface IProviderService<T> where T : class, new()
+    {
+        Method Method { get; }
+        Dictionary<string, string> Form { get; }
+        T Parser(string value);
+        string GetUrl(params string[] values);
+    }
+}
